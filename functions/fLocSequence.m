446c72ba09c40bb5444c2065763fe7c4458720e0
lapluis <stortinter@outlook.com>
2026-03-05 00:28:00 +0800

    mod: stim duration

--- functions/fLocSequence.m
+++ functions/fLocSequence.m
@@ -17,7 +17,7 @@ classdef fLocSequence
     properties (Constant)
         stim_conds = {'Bodies' 'Characters' 'Faces' 'Objects' 'Places'};
         stim_per_block = 12;   % number of stimuli in a block
-        stim_duty_cycle = 0.5; % duration of stimulus duty cycle (s)
+        stim_duty_cycle = 1;   % duration of stimulus duty cycle (s)
     end
     
     properties (Constant, Hidden)
